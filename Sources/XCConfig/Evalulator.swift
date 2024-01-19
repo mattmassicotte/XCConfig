6375b3d7ac16e5c4103c3cbe7b633411bee47d37
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-01-19 07:56:40 -0500

    Support root-relative includes

--- Sources/XCConfig/Evalulator.swift
+++ Sources/XCConfig/Evalulator.swift
@@ -2,6 +2,7 @@ import Foundation
 
 
 public struct BuildSettingsHeirarchy {
+	public let projectRootURL: URL
 	public let platformDefaults: [Assignment]
 	public let projectConfigURL: URL?
 	public let project: [Assignment]
@@ -9,12 +10,14 @@ public struct BuildSettingsHeirarchy {
 	public let target: [Assignment]
 
 	public init(
+		projectRootURL: URL,
 		platformDefaults: [Assignment],
 		projectConfigURL: URL?,
 		project: [Assignment],
 		configURL: URL?,
 		target: [Assignment]
 	) {
+		self.projectRootURL = projectRootURL
 		self.platformDefaults = platformDefaults
 		self.projectConfigURL = projectConfigURL
 		self.project = project
@@ -33,15 +36,19 @@ public struct Evaluator {
 	}
 
 	/// Resolve the contents of an xcconfifg file into an array of `Assignment`.
-	public func resolve(contentsOf url: URL) throws -> [Assignment] {
+	public func resolve(contentsOf url: URL, projectRoot: URL) throws -> [Assignment] {
 		var assignments = [Assignment]()
 
 		let statements = try Parser().parse(contentsOf: url)
 
 		for statement in statements {
 			switch statement {
-			case let .includeDirective(path), let .optionalIncludeDirective(path):
-				let includedAssignments = try resolveInclude(path, for: url)
+			case let .includeDirective(path):
+				let includedAssignments = try resolveInclude(path, for: url, rootURL: projectRoot)
+
+				assignments.append(contentsOf: includedAssignments)
+			case let .optionalIncludeDirective(path):
+				let includedAssignments = (try? resolveInclude(path, for: url, rootURL: projectRoot)) ?? []
 
 				assignments.append(contentsOf: includedAssignments)
 			case let .assignment(assignment):
@@ -68,24 +75,40 @@ public struct Evaluator {
 		return settings
 	}
 
-	private func resolveInclude(_ path: String, for fileURL: URL) throws -> [Assignment] {
-		let url = fileURL
+	/// Xcode's documentation (https://developer.apple.com/documentation/xcode/adding-a-build-configuration-file-to-your-project) says that this will always look up paths relative to the current file. However, it seems like if that fails, it will fall back to using the project root.
+	private func resolveInclude(_ path: String, for fileURL: URL, rootURL: URL) throws -> [Assignment] {
+		// first, try file-relative
+		let fileRelativeURL = fileURL
 			.deletingLastPathComponent()
 			.appendingPathComponent(path)
 
-		return try resolve(contentsOf: url)
+		if FileManager.default.isReadableFile(atPath: fileRelativeURL.path) {
+			return try resolve(contentsOf: fileRelativeURL, projectRoot: rootURL)
+		}
+
+		let rootRelativeURL = rootURL
+			.appendingPathComponent(path)
+
+		if FileManager.default.isReadableFile(atPath: rootRelativeURL.path) {
+			return try resolve(contentsOf: rootRelativeURL, projectRoot: rootURL)
+		}
+
+		let absoluteURL = URL(fileURLWithPath: path)
+
+		return try resolve(contentsOf: absoluteURL, projectRoot: rootURL)
 	}
 
 	/// Evaluate a heirarchy of setting entries into a single array.
 	public func evaluate(_ heirarchy: BuildSettingsHeirarchy) throws -> [BuildSetting: String] {
 		var assignments = heirarchy.platformDefaults
+		let root = heirarchy.projectRootURL
 
-		let projectAssignments = try heirarchy.projectConfigURL.map { try resolve(contentsOf: $0) } ?? []
+		let projectAssignments = try heirarchy.projectConfigURL.map { try resolve(contentsOf: $0, projectRoot: root) } ?? []
 
 		assignments.append(contentsOf: projectAssignments)
 		assignments.append(contentsOf: heirarchy.project)
 
-		let configAssignments = try heirarchy.configURL.map { try resolve(contentsOf: $0) } ?? []
+		let configAssignments = try heirarchy.configURL.map { try resolve(contentsOf: $0, projectRoot: root) } ?? []
 
 		assignments.append(contentsOf: configAssignments)
 		assignments.append(contentsOf: heirarchy.target)
--- Tests/XCConfigTests/EvaluatorTests.swift
+++ Tests/XCConfigTests/EvaluatorTests.swift
@@ -4,9 +4,10 @@ import XCConfig
 final class EvaluatorTests: XCTestCase {
 	func testEvaluateIncludedFile() throws {
 		let evaluator = Evaluator()
-		let url = try Bundle.module.testDataURL(named: "2.xcconfig")
+		let root = Bundle.module.testDataURL.appendingPathComponent("SingleFile")
+		let url = root.appendingPathComponent("Target.xcconfig")
 
-		let assignments = try evaluator.resolve(contentsOf: url)
+		let assignments = try evaluator.resolve(contentsOf: url, projectRoot: root)
 		let values = try evaluator.evaluate(assignments)
 
 		let expected: [BuildSetting : String] = [
@@ -18,9 +19,10 @@ final class EvaluatorTests: XCTestCase {
 
 	func testEvaluateSubdirectoryIncludedFile() throws {
 		let evaluator = Evaluator()
-		let url = try Bundle.module.testDataURL(named: "3.xcconfig")
+		let root = Bundle.module.testDataURL.appendingPathComponent("SubdirectoryInclude")
+		let url = root.appendingPathComponent("Target.xcconfig")
 
-		let assignments = try evaluator.resolve(contentsOf: url)
+		let assignments = try evaluator.resolve(contentsOf: url, projectRoot: root)
 		let values = try evaluator.evaluate(assignments)
 
 		let expected: [BuildSetting : String] = [
@@ -30,15 +32,31 @@ final class EvaluatorTests: XCTestCase {
 		XCTAssertEqual(values, expected)
 	}
 
-	func testEvaluateParentDirectoryIncludedFile() throws {
+	func testEvaluateRelativeDirectoryIncludedFile() throws {
 		let evaluator = Evaluator()
-		let url = try Bundle.module.testDataURL(named: "A/A3.xcconfig")
+		let root = Bundle.module.testDataURL.appendingPathComponent("RelativeDirectoryInclude")
+		let url = root.appendingPathComponent("A/Target.xcconfig")
 
-		let assignments = try evaluator.resolve(contentsOf: url)
+		let assignments = try evaluator.resolve(contentsOf: url, projectRoot: root)
 		let values = try evaluator.evaluate(assignments)
 
 		let expected: [BuildSetting : String] = [
-			BuildSetting(rawValue: "ENABLE_HARDENED_RUNTIME")! : "YES"
+			BuildSetting(rawValue: "ENABLE_HARDENED_RUNTIME")! : "NO"
+		]
+
+		XCTAssertEqual(values, expected)
+	}
+
+	func testEvaluateRootRelativeDirectoryIncludedFile() throws {
+		let evaluator = Evaluator()
+		let root = Bundle.module.testDataURL.appendingPathComponent("RootRelativeDirectoryInclude")
+		let url = root.appendingPathComponent("A/Target.xcconfig")
+
+		let assignments = try evaluator.resolve(contentsOf: url, projectRoot: root)
+		let values = try evaluator.evaluate(assignments)
+
+		let expected: [BuildSetting : String] = [
+			BuildSetting(rawValue: "ENABLE_HARDENED_RUNTIME")! : "NO"
 		]
 
 		XCTAssertEqual(values, expected)
@@ -49,6 +67,7 @@ extension EvaluatorTests {
 	func testPlatformDefaults() throws {
 		let evaluator = Evaluator()
 		let heirarchy = BuildSettingsHeirarchy(
+			projectRootURL: URL(fileURLWithPath: "/"),
 			platformDefaults: [.init(key: "ACTION", value: "HELLO")],
 			projectConfigURL: nil,
 			project: [],
@@ -68,6 +87,7 @@ extension EvaluatorTests {
 	func testProjectPrecedence() throws {
 		let evaluator = Evaluator()
 		let heirarchy = BuildSettingsHeirarchy(
+			projectRootURL: URL(fileURLWithPath: "/"),
 			platformDefaults: [.init(key: "ACTION", value: "HELLO")],
 			projectConfigURL: nil,
 			project: [.init(key: "ACTION", value: "GOODBYE")],
@@ -86,30 +106,13 @@ extension EvaluatorTests {
 
 	func testTargetFileOverridingProject() throws {
 		let evaluator = Evaluator()
+		let root = Bundle.module.testDataURL.appendingPathComponent("TargetOverride")
 		let heirarchy = BuildSettingsHeirarchy(
+			projectRootURL: root,
 			platformDefaults: [],
-			projectConfigURL: try Bundle.module.testDataURL(named: "A/A1.xcconfig"),
+			projectConfigURL: root.appendingPathComponent("Project.xcconfig"),
 			project: [],
-			configURL: try Bundle.module.testDataURL(named: "1.xcconfig"),
-			target: []
-		)
-
-		let values = try evaluator.evaluate(heirarchy)
-
-		let expected: [BuildSetting : String] = [
-			BuildSetting(rawValue: "ENABLE_HARDENED_RUNTIME")! : "YES"
-		]
-
-		XCTAssertEqual(values, expected)
-	}
-
-	func testTargetFileOverridingProjectWithInclude() throws {
-		let evaluator = Evaluator()
-		let heirarchy = BuildSettingsHeirarchy(
-			platformDefaults: [],
-			projectConfigURL: try Bundle.module.testDataURL(named: "3.xcconfig"),
-			project: [],
-			configURL: try Bundle.module.testDataURL(named: "1.xcconfig"),
+			configURL: root.appendingPathComponent("Target.xcconfig"),
 			target: []
 		)
 
